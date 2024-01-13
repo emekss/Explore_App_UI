cd14e0a78e4a76b529988fe0069e6b7eb2651736
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-01-13 03:10:26 +0100

    home screen updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.search_rounded),
+        actions: const [Icon(Icons.menu_rounded)],
+      ),
+    );
   }
 }
